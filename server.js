1e11daecc7ce67f79089ee0615523058098cd755
tai5863 <taisws5863@gmail.com>
2020-08-27 17:16:38 +0900

    [UPDATE]

--- server.js
+++ server.js
@@ -1,32 +1,64 @@
+let osc = require('node-osc');
+
 let express = require('express');
-  http = require('http');
+const http = require('http');
   url = require('url');
   path = require('path');
-  webSocket = require('ws');
+  WebSocket = require('ws');
 
 let app = express();
   server = http.createServer(app);
-  wss = new webSocket.Server({ server: server });
+  wss = new WebSocket.Server({ server: server });
 
 let connects = []
 
 app.use(express.static(path.join(__dirname, '/public')));
 
+let oscClient = new osc.Client('127.0.0.1', 6000);
+let oscServer = new osc.Server(3333, '0.0.0.0', () => {
+  console.log('OSC Server is listening');
+});
+
+oscServer.on('message', function(message) {
+  console.log('[OSC]Recieved: ');
+  console.log(message);
+
+  let sendMessage;
+
+  switch (message[0]) {
+    case '/address1':
+      console.log('address1');
+      broadcast('address1');
+      sendMessage = new osc.Message('/address1');
+      sendMessage.append('a');
+      oscClient.send(sendMessage);
+      break;
+    case '/address2':
+      console.log('address2');
+      broadcast('address2');
+      sendMessage = new osc.Message('/address2');
+      sendMessage.append('b');
+      oscClient.send(sendMessage);
+      break;
+    default:
+      break;
+  }
+});
+
 wss.on('connection', function(ws, req) {
   let location = url.parse(req.url, true);
-  
+
   let initMessage = { message: 'connection' };
   ws.send(JSON.stringify(initMessage));
   connects.push(ws);
-  console.log('New Client Connected: ' + connects.length);
+  console.log('[WS]New Client Connected: ' + connects.length);
 
   ws.on('message', function(message) {
-    console.log('recieved: %s', message);
-    broadcast(message);
+    console.log('[WS]Recieved: %s', message);
   });
 
   ws.on('close', function() {
-    console.log('Client Leave');
+    console.log('[WS]Client Leave');
     connects = connects.filter(function(conn, i) {
       return (conn == ws) ? false : true;
     });
